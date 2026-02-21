9c27329f0e85d3c88f335fcde58c3a2e5fa11437
CiccioGamer00 <stemoro84@libero.it>
2026-02-21 11:37:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 window.API_CONFIG = {
   baseUrl: "https://calcio-report-proxy.stemoro84.workers.dev",
   headers: {},
+  // Imposta qui i tuoi contatti
+  telegramUrl: "", // es: "https://t.me/tuo_canale"
+  supportEmail: "", // es: "support@tuodominio.it"
 };
